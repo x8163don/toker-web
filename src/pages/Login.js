007225f043f2c9e686610a9d90de7823cf62bf57
Bevis <x8163don@gmail.com>
2023-04-17 14:03:00 +0800

    feat: add tailwindcss

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,7 @@ import React from "react";
 import {useFormik} from "formik";
 import * as Yup from "yup";
 import axios, {HttpStatusCode} from "axios";
+import {Link} from "react-router-dom";
 
 const LoginPage = () => {
     const formik = useFormik({
@@ -10,10 +11,8 @@ const LoginPage = () => {
             password: "",
         },
         validationSchema: Yup.object({
-            email: Yup.string().email("無效的 Email 格式").required("必填"),
-            password: Yup.string()
-                .min(6, "密碼至少需要 6 個字元")
-                .required("必填"),
+            email: Yup.string().email("Email 格式不正確").required("請填寫帳號"),
+            password: Yup.string().required("請填寫密碼"),
         }),
         onSubmit: async (values) => {
             const response = await axios.post("accounts:login", {
@@ -28,40 +27,53 @@ const LoginPage = () => {
     });
 
     return (
-        <form onSubmit={formik.handleSubmit}>
-            <div>
-                <label htmlFor="email">帳號：</label>
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    onChange={formik.handleChange}
-                    value={formik.values.email}
-                    onBlur={formik.handleBlur}
-                />
-                {formik.touched.email && formik.errors.email ? (
-                    <div>{formik.errors.email}</div>
-                ) : null}
-            </div>
+        <div className="mx-auto md:h-screen flex flex-col justify-center items-center px-6 pt-8 pt:mt-0">
+            <Link to="/" class="text-2xl font-semibold flex justify-center items-center mb-8 lg:mb-10">
+                <img src="/images/logo.svg" className="h-10 mr-4" alt="Logo"/>
+                <span className="self-center text-2xl font-bold whitespace-nowrap">拓客</span>
+            </Link>
 
-            <div>
-                <label htmlFor="password">密碼：</label>
-                <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    onChange={formik.handleChange}
-                    value={formik.values.password}
-                    onBlur={formik.handleBlur}
-                />
-                {formik.touched.password && formik.errors.password ? (
-                    <div>{formik.errors.password}</div>
-                ) : null}
+            <div className="bg-white shadow rounded-lg md:mt-0 w-full sm:max-w-screen-sm xl:p-0">
+                <div className="p-6 sm:p-8 lg:p-16 space-y-8">
+                    <h2 className="text-2xl lg:text-3xl font-bold text-gray-900"> 登入平台</h2>
+                    <form className="mt-8 space-y-6" onSubmit={formik.handleSubmit}>
+                        <div>
+                            <label htmlFor="email" className="text-sm font-medium text-gray-900 block mb-2">帳號</label>
+                            <input
+                                type="email"
+                                id="email"
+                                name="email"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
+                                placeholder="name@company.com" required
+                                onChange={formik.handleChange}
+                                value={formik.values.email}
+                                onBlur={formik.handleBlur}
+                            />
+                            <label className="mt-1 h-1 text-sm font-medium text-red-700 block mb-2">{formik.touched.email && formik.errors.email ? (
+                                <div>• {formik.errors.email}</div>) : <div></div>}
+                            </label>
+                        </div>
+                        <div>
+                            <label htmlFor="password"
+                                   className="text-sm font-medium text-gray-900 block mb-2">密碼</label>
+                            <input type="password" name="password" id="password" placeholder="••••••••"
+                                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
+                                   required
+                                   onChange={formik.handleChange}
+                                   value={formik.values.password}
+                                   onBlur={formik.handleBlur}/>
+                            <label className="mt-1 h-1 text-sm font-medium text-red-700 block mb-2">
+                                {formik.touched.password && formik.errors.password ? (
+                                    <div>• {formik.errors.password}</div>) : <div></div>}
+                            </label>
+                        </div>
+                        <button type="submit"
+                                className="text-white bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:ring-cyan-200 font-medium rounded-lg text-base px-5 py-3 w-full sm:w-auto text-center">登入
+                        </button>
+                    </form>
+                </div>
             </div>
-
-            <button type="submit">登入</button>
-        </form>
-    );
+        </div>);
 };
 
 export default LoginPage;
