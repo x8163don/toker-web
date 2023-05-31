8d4f465c5c535e7ebc00eacbfe6af60535533aff
Bevis <x8163don@gmail.com>
2023-05-31 13:27:23 +0800

    fix: remove useless import

--- src/components/customers/CustomersList.js
+++ src/components/customers/CustomersList.js
@@ -1,4 +1,4 @@
-import React, {useNavigate} from 'react';
+import React from 'react';
 import {Table} from 'flowbite-react';
 import {HiOutlineTrash} from "react-icons/hi";
 import {Link} from "react-router-dom";
