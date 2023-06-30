051617a57e01013ce3922620b70b1f1aba1e5121
Bevis <x8163don@gmail.com>
2023-06-30 11:10:43 +0800

    fix: typo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import './App.css';
-import {RouterProvider} from "react-router-dom";
-import {router} from "./pages/Router";
+import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./pages/Router";
 import axios from "axios";
-import {AuthContextProvider} from "./store/AuthContext";
+import { AuthContextProvider } from "./store/AuthContext";
 
 function App() {
-    axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
 
-    return <AuthContextProvider>
-        <RouterProvider router={router}/>;
+  return (
+    <AuthContextProvider>
+      <RouterProvider router={router} />
     </AuthContextProvider>
+  );
 }
 
 export default App;
